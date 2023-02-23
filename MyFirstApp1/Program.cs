aa9c29270fc6d8df9ede80e39639d408ad34014c
Igor Pr <igor.prokofjev@tktk.ee>
2023-02-23 14:38:30 +0200

    create new app

---
+++ MyFirstApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//rakendus palub kasutajat sisestada tema nimi
+//rakendus tervitab nimepidi
+
+
+
+Console.WriteLine("Enter your name");
+//string - sõne
+string username = Console.ReadLine();
+Console.WriteLine("Hello" + ", " + username + "!");
+
+ //string interpolation
+Console.WriteLine($"Hello, {username}!");
